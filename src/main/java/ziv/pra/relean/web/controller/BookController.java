cadd12c7ca4d342380d704a4fa43bf802cbff9a9
里夫 <zivchan.tw@gmail.com>
2021-02-03 19:56:28 +0800

    根據是否更新跳出提示訊息及redirect參數處理

--- src/main/java/ziv/pra/relean/web/controller/BookController.java
+++ src/main/java/ziv/pra/relean/web/controller/BookController.java
@@ -1,11 +1,13 @@
 package ziv.pra.relean.web.controller;
 
+import org.dom4j.rule.Mode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import ziv.pra.relean.web.model.Book;
 import ziv.pra.relean.web.service.BookService;
 
@@ -60,7 +62,22 @@ public class BookController {
      * @return
      */
     @GetMapping("/book/input")
-    public String pageInput() {
+    public String pageInput(Model model) {
+        model.addAttribute("book", new Book());
+        return "input";
+    }
+
+    /**
+     * 跳到更新頁面
+     *
+     * @param id
+     * @param model
+     * @return
+     */
+    @GetMapping("/book/{id}/input")
+    public String inputEdit(@PathVariable long id, Model model) {
+        Optional<Book> opBook = bookService.findOne(id);
+        model.addAttribute("book", opBook.get());
         return "input";
     }
 
@@ -70,9 +87,14 @@ public class BookController {
      * @param book
      * @return
      */
+    //因為是redirect的值，所以等於是用這個api去打get的那隻api，但model只做用於這個api內，所以要改成RedirectAttributes
+    //方法也要換成addFlashAttribute才會起作用
     @PostMapping("/books")
-    public String postSumit(Book book) {
-        bookService.save(book);
+    public String postSumit(Book book, final RedirectAttributes model) {
+        Book opBook = bookService.save(book);
+        if (opBook != null) {
+            model.addFlashAttribute("message", "新增" + book.getName() + "成功");
+        }
         return "redirect:/books";
     }
 }
